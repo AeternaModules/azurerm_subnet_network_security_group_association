531ab1f3406b942984a1995f29fd730c0e1054b8
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:51:33 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
 output "subnet_network_security_group_associations_id" {
   description = "Map of id values across all subnet_network_security_group_associations, keyed the same as var.subnet_network_security_group_associations"
-  value       = { for k, v in azurerm_subnet_network_security_group_association.subnet_network_security_group_associations : k => v.id }
+  value       = { for k, v in azurerm_subnet_network_security_group_association.subnet_network_security_group_associations : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "subnet_network_security_group_associations_network_security_group_id" {
   description = "Map of network_security_group_id values across all subnet_network_security_group_associations, keyed the same as var.subnet_network_security_group_associations"
-  value       = { for k, v in azurerm_subnet_network_security_group_association.subnet_network_security_group_associations : k => v.network_security_group_id }
+  value       = { for k, v in azurerm_subnet_network_security_group_association.subnet_network_security_group_associations : k => v.network_security_group_id if v.network_security_group_id != null && length(v.network_security_group_id) > 0 }
 }
 output "subnet_network_security_group_associations_subnet_id" {
   description = "Map of subnet_id values across all subnet_network_security_group_associations, keyed the same as var.subnet_network_security_group_associations"
-  value       = { for k, v in azurerm_subnet_network_security_group_association.subnet_network_security_group_associations : k => v.subnet_id }
+  value       = { for k, v in azurerm_subnet_network_security_group_association.subnet_network_security_group_associations : k => v.subnet_id if v.subnet_id != null && length(v.subnet_id) > 0 }
 }
 
